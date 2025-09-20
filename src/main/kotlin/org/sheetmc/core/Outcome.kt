fe77aafe05fb89683a14ce906fdd2fbd20780771
mtctx <me@mtctx.dev>
2025-09-20 23:30:46 +0200

    Fixed clashing with Kotlin built in types

--- src/main/kotlin/org/sheetmc/core/Outcome.kt
+++ src/main/kotlin/org/sheetmc/core/Outcome.kt
@@ -1,5 +1,5 @@
 /*
- *     SheetMC Core: Result.kt
+ *     SheetMC Core: Outcome.kt
  *     Copyright (C) 2025 SheetMC.org
  *
  *     This program is free software: you can redistribute it and/or modify
@@ -18,26 +18,26 @@
 
 package org.sheetmc.core
 
-sealed class Result<out T> {
-    data class Success<T>(val value: T) : Result<T>()
-    data class Error(val message: String, val throwable: Throwable? = null) : Result<Nothing>()
+sealed class Outcome<out T> {
+    data class Success<T>(val value: T) : Outcome<T>()
+    data class Error(val message: String, val throwable: Throwable? = null) : Outcome<Nothing>()
 }
 
 class ErrorBuilder() {
     var message: String = "No message provided."
     var throwable: Throwable? = null
 
-    fun build(): Result.Error = Result.Error(message, throwable)
+    fun build(): Outcome.Error = Outcome.Error(message, throwable)
 }
 
-fun Okay(): Result.Success<Boolean> = Result.Success(true)
-fun <T> Okay(type: T): Result.Success<T> = Result.Success(type)
-fun <T> Okay(block: () -> T): Result.Success<T> {
-    return Result.Success(block())
+fun okay(): Outcome.Success<Boolean> = Outcome.Success(true)
+fun <T> okay(type: T): Outcome.Success<T> = Outcome.Success(type)
+fun <T> okay(block: () -> T): Outcome.Success<T> {
+    return Outcome.Success(block())
 }
 
-fun Error(message: String, throwable: Throwable? = null): Result.Error = Result.Error(message, throwable)
-fun Error(block: ErrorBuilder.() -> Unit): Result.Error {
+fun failed(message: String, throwable: Throwable? = null): Outcome.Error = Outcome.Error(message, throwable)
+fun failed(block: ErrorBuilder.() -> Unit): Outcome.Error {
     val builder = ErrorBuilder()
     block(builder)
     return builder.build()
